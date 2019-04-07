7f3f7745ab255e63d5b1e2a80d6f60a246f58675
KononenkoAsya <ananaskononenko@gmail.com>
2019-04-07 22:07:08 +0300

    Using Lodash for search in the array

--- src/hocs/hoc.js
+++ src/hocs/hoc.js
@@ -1,12 +1,23 @@
 import React from 'react'
+const _ = require('lodash')
 
 const Hoc = WrappedComponent => {
   const users = ['User1', 'User2', 'User3', 'User4', 'User5', 'User6']
-  const randomUser = users[Math.floor(Math.random() * 6)]
+
+  const dropFirstUser = _.drop(users, 1)
+  const dropLastUser = _.dropRight(dropFirstUser, 1)
+  const allButTheLast = _.initial(dropLastUser)
+  const lastElem = _.last(allButTheLast)
+  const concatUsers = _.concat(users, lastElem)
+  const pullUser = _.pull(concatUsers, 'User4')
+  const reverseUsers = _.reverse(pullUser)
+  const takeElementsUsers = _.take(reverseUsers, 5)
+
+  const user = _.head(takeElementsUsers)
 
   return props => (
     <div className='userBlock'>
-      <WrappedComponent text={randomUser} />
+      <WrappedComponent text={user} />
     </div>
   )
 }
